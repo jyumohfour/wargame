73f8526d045c0492c71900f61eb583099d741bbd
jyumohfour <jsya2020@mymail.pomona.edu>
2022-05-27 21:19:06 -0700

    Initial Commit

---
+++ warthing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warthing
+//
+//  Created by Fresh Prince of Bel-Air on 27/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warthing/warthingApp.swift
@@ -0,0 +1,17 @@
+//
+//  warthingApp.swift
+//  warthing
+//
+//  Created by Fresh Prince of Bel-Air on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct warthingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
